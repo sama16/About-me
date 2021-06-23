224f367abba9b7a729963099d4082d7d484bd4d6
sama16 <samaalmallak7@gmail.com>
2021-06-24 02:33:47 +0300

    add a tag to App.js

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import sama from"./sama.jpg";
+import"./App.css" ;
+function About() {
+    return (
+      <div>
+<img src={sama}/>;
+  <h1>sama</h1>
+      </div>
+    );
+  }
+export default About; 
